2f7e67c58e2a2229fb298b244d651e4d82239f49
Jeff Huang <jeff@worldblender.com>
2010-11-16 01:24:35 +0000

    distance, duration

--- models.js
+++ models.js
@@ -5,16 +5,41 @@ var app = require('./app');
 var default_port = mongodb.Connection.DEFAULT_PORT;
 var db = new mongodb.Db(APP_NAME, new mongodb.Server('localhost', default_port, {}), {native_parser: true});
 
-var TODO_SECONDS = 5;
+// configuration
 var MISSILE_RADIUS = 300; // in meters
+var STARTING_HP = 100;
+var MISSILE_VELOCITY = 2;
+var MISSILE_ACCELERATION = 0.00868;
 
+// physical constants
+var RAD_TO_METERS = 6371 * 1000;
+
+// connect to mongodb
 db.open(function(err, db) {
   console.log("connected to mongodb");
 });
 
+// useful functions
+function printObject(o) {
+  var output = '';
+  for (property in o) {
+    output += property + ': ' + o[property]+'; ';
+  }
+  return output;
+}
+
 exports.clearDb = function() {
   db.dropDatabase(function(err, result) {});
 };
+
+function haversineDistance(coords1, coords2) {
+  var dLat = (coords2.lat-coords1.lat) * Math.PI / 180;
+  var dLon = (coords2.long-coords1.long) * Math.PI / 180;
+  var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(coords1.lat * Math.PI / 180) * Math.cos(coords2.lat * Math.PI / 180) * Math.sin(dLon/2) * Math.sin(dLon/2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  var d = RAD_TO_METERS * c;
+  return d;  
+}
 
 // Models
 exports.Coords = function(long, lat) {
@@ -22,29 +47,30 @@ exports.Coords = function(long, lat) {
   this.lat = lat;
 }
 
-var RAD_TO_METERS = 6371 * 1000;
 
 exports.Player = function(username, coords) {
   this._id = username; // TODO(jeff): check uniqueness
-  this.hp = 100;
+  this.hp = STARTING_HP;
   this.coords = coords; // TODO(jeff): check validity
   this.weapons = ['missile'];
-
-  var p = this;
-  db.players.insert(p);
+  db.players.insert(this);
 }
 
 exports.Missile = function(username, arrivalCoords) {
   this.owner = username;
   var m = this;
   m.departureTime = Date.parse(new Date());
-  m.arrivalTime = m.departureTime + TODO_SECONDS * 1000;
   m.arrivalCoords = arrivalCoords; // TODO(jeff): check validity
   db.players.findOne({_id: username}, function(err, document) {
     // TODO(jeff): check for error
     m.departureCoords = document.coords;
 
+    var distance = haversineDistance(m.arrivalCoords, m.departureCoords);
+    var duration = (-MISSILE_VELOCITY + Math.sqrt(MISSILE_VELOCITY * MISSILE_VELOCITY + 2 * MISSILE_ACCELERATION * distance)) / MISSILE_ACCELERATION;
+    m.arrivalTime = m.departureTime + duration * 1000;
+
     db.missiles.insert(m, function(err, docs) {
+      console.log("lauched missile; " + printObject(m));
       setTimeout(function() {missileArrived(docs[0]);}, m.arrivalTime - Date.parse(new Date()));
     });
   });
--- test/app.test.js
+++ test/app.test.js
@@ -24,17 +24,18 @@ setTimeout(function() {
     var p, m;
     p = new models.Player("jeff", new models.Coords(-122.328472, 47.622682));
     m = new models.Missile("jeff", new models.Coords(-122.327099, 47.605006));
-    p = models.Player("coffee1", new models.Coords(-122.329779, 47.604359));
-    p = models.Player("coffee2", new models.Coords(-122.329798, 47.604402));
-    p = models.Player("coffee3", new models.Coords(-122.329599, 47.602469));
-    p = models.Player("coffee4", new models.Coords(-122.330905, 47.604007));
-    p = models.Player("coffee5", new models.Coords(-122.33046, 47.60506));
-    p = models.Player("coffee6", new models.Coords(-122.331462, 47.602865));
-    p = models.Player("coffee7", new models.Coords(-122.329689, 47.60146));
-    p = models.Player("coffee8", new models.Coords(-122.332252, 47.603602));
+    p = new models.Player("coffee1", new models.Coords(-122.329779, 47.604359));
+    p = new models.Player("coffee2", new models.Coords(-122.329798, 47.604402));
+    p = new models.Player("coffee3", new models.Coords(-122.329599, 47.602469));
+    p = new models.Player("coffee4", new models.Coords(-122.330905, 47.604007));
+    p = new models.Player("coffee5", new models.Coords(-122.33046, 47.60506));
+    p = new models.Player("coffee6", new models.Coords(-122.331462, 47.602865));
+    p = new models.Player("coffee7", new models.Coords(-122.329689, 47.60146));
+    p = new models.Player("coffee8", new models.Coords(-122.332252, 47.603602));
     m = new models.Missile("coffee3", new models.Coords(-122.327093, 47.605002));
     m = new models.Missile("coffee3", new models.Coords(-122.324099, 47.605106));
     m = new models.Missile("coffee6", new models.Coords(-122.337099, 47.602006));
+    p.move(new models.Coords(-122.31923, 47.603283));
   };
   exports.indexPage = function(assert) {
     assert.response(app,
