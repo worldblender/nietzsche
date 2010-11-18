3ae263408905075d175d4a4d7280fde3904f5346
Jeff Huang <jeff@worldblender.com>
2010-11-18 10:51:43 +0000

    start work on menu functionality

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ app.configure(function(){
   app.set('views', __dirname + '/views');
   app.use(express.bodyDecoder());
   app.use(express.methodOverride());
-  app.use(express.compiler({ src: __dirname + '/public' }));
   app.use(app.router);
   app.use(express.staticProvider(__dirname + '/public'));
   app.set('view options', { layout: false });
